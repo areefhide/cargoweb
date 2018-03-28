bc427b04e60a1a7ff1d2444485dbb0fbaf749c13
arif <areef.hide@gmail.com>
2018-03-28 16:11:02 +0000

    company

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -8,7 +8,10 @@ export class CompanyService {
 
   constructor(private http: HttpClient) { }
   create(companyName: string){
-    
+     return this.http.post<any>('http://localhost:3000/api/company',{nama: companyName})
+     .map(data =>{
+       return data;
+     });
   }
 
 }
