ab60fdf6144ef0121123861919cc334f30b5d3c5
arif hidayat <areef.hide@gmail.com>
2018-04-03 23:17:30 +0700

    pengiriman

--- src/app/class/paket.ts
+++ src/app/class/paket.ts
@@ -1,4 +1,14 @@
 export class Paket {
     company: String;
-    
+    pengirim: String;
+    asal: String;
+    agen: String;
+    penerima: String;
+    tujuan: String;
+    tanggal: Date;
+    biayakirim: Number;
+    biayatambahan: Number;
+    projectId: String;
+    status: String;
+    islunas: Boolean;
 }
