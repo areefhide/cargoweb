906c10e296da792fba49b102b429f14527989075
arif hidayat <areef.hide@gmail.com>
2018-04-08 16:26:34 +0700

    kurir

--- src/app/components/kurir/kurir/kurir.component.ts
+++ src/app/components/kurir/kurir/kurir.component.ts
@@ -47,7 +47,7 @@ export class KurirComponent implements OnInit {
       return data;
     }).subscribe(data=>{
       this.user = data.data as User;
-      this.kurir.userId = this.user._id;
+      this.kurir.username = this.user.username;
       this.createKurir();
     });
   }
