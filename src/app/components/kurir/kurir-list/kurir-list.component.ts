d7c980022d8675a156f93218d75fda68953284c3
arif hidayat <areef.hide@gmail.com>
2018-04-10 16:41:29 +0700

    kurir

--- src/app/components/kurir/kurir-list/kurir-list.component.ts
+++ src/app/components/kurir/kurir-list/kurir-list.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { AppSettings } from '../../../class/app-settings';
 import { ResponseData } from '../../../class/response';
+import { ResponseCreate } from '../../../class/response-create';
 
 @Component({
   selector: 'app-kurir-list',
@@ -14,6 +15,8 @@ import { ResponseData } from '../../../class/response';
 export class KurirListComponent implements OnInit {
 
   kurir: any[];
+  page: Number = 1;
+  maxPage:Number = 10;
   constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
@@ -21,13 +24,39 @@ export class KurirListComponent implements OnInit {
   }
 
   private loadAllData(){
-    this.http.get<ResponseData>(AppSettings.API_ENDPOINT + 'kurir')
+    this.http.get<ResponseData>(AppSettings.API_ENDPOINT + 'kurir?page='+ this.page.toString())
     .map(data=>{
       return data.data;
     }).subscribe(data =>
     {
       this.kurir = data.docs;
+      this.page = data.page;
+      this.maxPage = data.pages;
       console.log(this.kurir);
     });
+  }
+  prev(){
+    var page = (this.page > 1)? (this.page as number) -1: 1;
+    this.page = page;
+    this.loadAllData();
+  }
+  next(){
+    var page = (this.page < this.maxPage)? (this.page as number) +1: this.maxPage;
+    this.page = page;
+    this.loadAllData();
+  }
+
+  editKurir(id: string){
+    
+  }
+  deleteKurir(id: string){
+    this.http.delete<ResponseCreate>(AppSettings.API_ENDPOINT + 'kurir/' + id)
+    .map(data=>{
+      return data;
+    }).subscribe(
+      data=>{
+        this.loadAllData();
+      }
+    )
   }
 }
